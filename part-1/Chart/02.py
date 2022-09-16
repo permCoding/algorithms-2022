048742d5cf8642dea664095f4212a34b9ba2a5fd
Андрей Беляков <ttxiom@gmail.com>
2022-09-16 06:54:53 +0500

    рекурсия

--- part-1/Chart/02.py
+++ part-1/Chart/02.py
@@ -2,16 +2,12 @@ import matplotlib.pyplot as plt
 
 
 def func(x):
-    '''ваша функция'''
     return 9.5*x**3 - 500
 
 
-styles = plt.style.available
-print(styles)
-plt.style.use(styles[1])
-# plt.style.use('seaborn')
+plt.style.use('seaborn')
 
-left, right, count = -5, 5, 100
+left, right, count = -5, 5, 1000
 x, step = left, (right - left) / count
 lst_x, lst_y = [x], [func(x)]
 while x < right:
@@ -20,9 +16,8 @@ while x < right:
     lst_y.append(func(x))
 
 fig, axes = plt.subplots()
-
-axes.plot(lst_x, lst_y, color='darkviolet', lw=4)
 axes.vlines(0, min(lst_y), max(lst_y), color='orange', lw=2)
 axes.hlines(0, min(lst_x), max(lst_x), color='orange', lw=2)
+axes.plot(lst_x, lst_y, color='darkviolet', lw=4)
 
 plt.show()
--- part-1/Chart/04.py
+++ part-1/Chart/04.py
@@ -2,21 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
   
+# Get the angles from 0 to 2 pie (360 degree) in narray object
 X = np.arange(0, math.pi*2, 0.05)
-Y1, Y2, Y3, Y4 = np.sin(X), np.cos(X), np.tan(X), np.tanh(X)
   
-figure, axis = plt.subplots(2, 2)  # разбить оси по строкам и столбцам
+# Using built-in trigonometric function we can directly plot
+# the given cosine wave for the given angles
+Y1 = np.sin(X)
+Y2 = np.cos(X)
+Y3 = np.tan(X)
+Y4 = np.tanh(X)
   
+# Initialise the subplot function using number of rows and columns
+figure, axis = plt.subplots(2, 2)
+  
+# For Sine Function
 axis[0, 0].plot(X, Y1)
-axis[0, 0].set_title("Синус")
+axis[0, 0].set_title("Sine Function")
   
+# For Cosine Function
 axis[0, 1].plot(X, Y2)
-axis[0, 1].set_title("Косинус")
+axis[0, 1].set_title("Cosine Function")
   
+# For Tangent Function
 axis[1, 0].plot(X, Y3)
-axis[1, 0].set_title("Тангенс")
+axis[1, 0].set_title("Tangent Function")
   
+# For Tanh Function
 axis[1, 1].plot(X, Y4)
-axis[1, 1].set_title("Арктангенс")
+axis[1, 1].set_title("Tanh Function")
   
+# Combine all the operations and display
 plt.show()
