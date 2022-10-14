4373d1149580e4d9f10f05cc85b3152f7d0df790
Андрей Беляков <ttxiom@gmail.com>
2022-10-14 12:23:42 +0500

    up

--- part-1/theme-02-find/00_chart/01.py
+++ part-1/theme-02-find/00_chart/01.py
@@ -1,3 +1,5 @@
+from cProfile import label
+from turtle import color, title
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -20,13 +22,15 @@ def func_D(lst_x):
 
 fig, axes = plt.subplots()
 axes.grid(True, linestyle='-.')
-axes.tick_params(labelcolor='r', labelsize='medium', width=3)
+axes.tick_params(labelcolor='b', labelsize='medium', width=3)
 axes.set_title("Сравнение функций")
+clr_labels = ['red', 'green', 'orange', "black"]
 
 lst_x = np.linspace(1., 3.*np.pi, 100)
-plt.plot(lst_x, func_A(lst_x))
-plt.plot(lst_x, func_B(lst_x))
-plt.plot(lst_x, func_C(lst_x))
-plt.plot(lst_x, func_D(lst_x), color="black", dashes=[6,2], linewidth=3)
+plt.plot(lst_x, func_A(lst_x), color=clr_labels[0], label="sin(x)")
+plt.plot(lst_x, func_B(lst_x), color=clr_labels[1], label="cos(x)")
+plt.plot(lst_x, func_C(lst_x), color=clr_labels[2], label="1/3*sin(x)+2*cos(x)")
+plt.plot(lst_x, func_D(lst_x), color=clr_labels[3], dashes=[6,2], linewidth=3, label="-1 + log(x) - x/5")
+plt.legend(title="Function colors")
 
 plt.show()
