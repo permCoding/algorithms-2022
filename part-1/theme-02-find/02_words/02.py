ef873ec52b30bfd86a10ee574129dfe41d1f62ea
Андрей Беляков <ttxiom@gmail.com>
2022-10-16 22:40:51 +0500

    labrab bin find

--- part-1/theme-02-find/02_words/02.py
+++ part-1/theme-02-find/02_words/02.py
@@ -1,4 +1,5 @@
 from time import monotonic
+from os import system
 
 
 def find_line(fw):
@@ -12,8 +13,8 @@ def find_bin_rec(fw, x, y):  # recursia
     if x == y and words[x] != fw: return -1
     m = x + (y-x)//2
     if fw == words[m]: return m
-    if fw > words[m]: return find_bin(fw,m+1,y)
-    if fw < words[m]: return find_bin(fw,x,m-1)
+    if fw > words[m]: return find_bin_rec(fw, m+1, y)
+    if fw < words[m]: return find_bin_rec(fw, x, m-1)
 
 
 def find_bin(fw, x, y):  # без рекурсии - циклом
@@ -24,19 +25,21 @@ def find_bin(fw, x, y):  # без рекурсии - циклом
     return -1
 
 
+system("cls")
+
 words = open("./words.csv", mode="r", encoding="windows-1251").read().split('\n')
 
 find_words = [
     "ящик", "абак", "кукуруза", "комарик", "як", 
     "ящик", "яхта", "яхта", "яхта", "яхта"
 ]
-find_words *= 50
+find_words *= 100  # так много слов, чтобы измерить время
 
 a = monotonic()
-# results = [find_line(word) for word in find_words]  # 8.7 sek
-results = [find_bin_rec(word,0,len(words)-1) for word in find_words]  # 0.007 sek
-# results = [find_bin(word) for word in find_words]  # проведите замеры своей функции
+results = [find_line(word) for word in find_words]  # ??? sek
+# results = [find_bin_rec(word,0,len(words)-1) for word in find_words]  # ??? sek
+# results = [find_bin(word) for word in find_words]  # проведите замеры всех трёх функций
 b = monotonic()
 
-print(round(b-a, 3))
-print(results)
+print(round(b-a, 3), "sek")
+print(results[:10])
