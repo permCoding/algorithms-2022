3723b82ccc03280995b4c0ffa892448008d1fa2d
Андрей Беляков <ttxiom@gmail.com>
2022-11-11 09:58:27 +0500

    lect 2022 11 11

---
+++ part-1/theme-02-find/03_in_str/app.js
@@ -0,0 +1,27 @@
+let { readFileSync } = require("fs");
+let { gpn, gpe, gps, gpb } = require("./module");
+
+console.clear();
+
+let txt = readFileSync("./txt/book.txt", "utf8");
+let word = "кора";
+
+console.time("find");
+console.log(gpn(txt, word));
+console.timeEnd("find");
+
+console.time("find");
+console.log(gpe(txt, word));
+console.timeEnd("find");
+
+console.time("find");
+console.log(gps(txt, word)); // Алгоритм Бойера-Мура
+console.timeEnd("find");
+
+console.time("find");
+console.log(gpb(txt, word)); // Алгоритм Бойера-Мура
+console.timeEnd("find");
+
+console.time("find");
+console.log(txt.indexOf(word));
+console.timeEnd("find");
---
+++ part-1/theme-02-find/03_in_str/module.js
@@ -0,0 +1,80 @@
+const get_pos_niave = (txt, word) => {
+    let lw = word.length;
+    for (let pos=0; pos<txt.length-lw; pos++) {
+        let b = true;
+        for (let i=0; i<lw; i++) {
+            if (word[i] !== txt[pos+i]) {
+                b = false;
+                break;
+            }
+        }
+        if (b === true) { return pos; } 
+    }
+    return -1;
+}
+
+const get_pos_equel = (txt, word) => {
+    let lw = word.length;
+    for (let pos=0; pos<txt.length; pos++) {
+        if (word === txt.slice(pos, pos+lw)) {
+            return pos;
+        }
+    }
+    return -1;
+}
+
+let get_dict_set = (txt, word) => {
+    let st = new Set(txt);
+    let dct = {};
+    let lw = word.length;
+    for (let smb of st) { dct[smb] = lw; }
+    for (let i=0; i<lw-1; i++) {
+        dct[word[i]] = lw-1 - i; // qwerty  для e => i=2 lw=6 sm=3
+    }
+    return dct;
+}
+
+let get_dict_for = (txt, word) => {
+    let dct = {};
+    let lw = word.length;
+    for (let smb of txt) { dct[smb] = lw; }
+    for (let i=0; i<lw-1; i++) {
+        dct[word[i]] = lw-1 - i; // qwerty  для e => i=2 lw=6 sm=3
+    }
+    return dct;
+}
+
+let find_pos_bm_set = (txt, word, dct) => {
+    let lw = word.length;
+    let pos=0;
+    while (pos < txt.length-lw) {
+        let b = true;
+        for (let i=0; i<lw; i++) {
+            if (word[i] !== txt[pos+i]) {
+                b = false;
+                pos += dct[txt[pos+lw-1]];
+                break;
+            }
+        }
+        if (b === true) { return pos; } 
+    }
+    return -1;
+}
+
+const get_pos_bm_set = (txt, word) => {
+    // let dct = get_dict_set(txt, word); // { 'к': 3, 'р': 1, 'а': 2, '#': 4, '&': 4 }
+    let dct = get_dict_for(txt, word);
+    return find_pos_bm_set(txt, word, dct);
+}
+
+const get_pos_bm = (txt, word) => {
+    // let dct = get_dict(word); // { 'к': 3, 'р': 1, 'а': 2 }
+    // return find_pos_bm(txt, word, dct);
+}
+
+module.exports = {
+    gpn: get_pos_niave,
+    gpe: get_pos_equel,
+    gps: get_pos_bm_set,
+    gpb: get_pos_bm,
+}
